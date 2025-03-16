96e143ef54155f2957b9d19304a237ede3cad57d
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-03-16 17:36:54 +0530

    modify button style

--- src/index.js
+++ src/index.js
@@ -9,17 +9,30 @@ const about = document.querySelector("#about")
 const content = document.querySelector("#content")
 
 loadPage()
+home.style = "background-color: white; color:black";
 
 home.addEventListener("click", ()=>{
+    home.style = "background-color: white; color:black";
+    menu.style = "background-color: rgba(255, 255, 255, 0); color:white";
+    about.style = "background-color: rgba(255, 255, 255, 0); color:white";
+
     content.innerHTML = ""
     loadPage()
 })
 
 menu.addEventListener("click", () => {
+    menu.style = "background-color: white; color:black";
+    about.style = "background-color: rgba(255, 255, 255, 0); color:white";
+    home.style = "background-color: rgba(255, 255, 255, 0); color:white";
+
     content.innerHTML = ""
     getMenuPage()
 })
 about.addEventListener("click", () => {
+    about.style = "background-color: white; color:black";
+    menu.style = "background-color: rgba(255, 255, 255, 0); color:white";
+    home.style = "background-color: rgba(255, 255, 255, 0); color:white";
+    
     content.innerHTML = ""
     getAboutPage()
 })
