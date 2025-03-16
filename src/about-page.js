9d642350a4bcadfe5ba7e48f68b1c92151befdf2
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-03-16 17:35:47 +0530

    add about page content

--- src/about-page.js
+++ src/about-page.js
@@ -1,11 +1,23 @@
 export function getAboutPage(){
     const content = document.querySelector("#content")
+
+    const aboutContainer = document.createElement("div")
+    const aboutTextContainer = document.createElement("div")
+    aboutContainer.classList.add("about-container")
+    aboutTextContainer.classList.add("about-text-container")
+
     const header = document.createElement("header")
-    const text = document.createElement("div")
+    const firstText = document.createElement("p")
+    const secondText = document.createElement("p")
 
-    header.textContent = "About"
-    text.textContent = "This is our about page"
+    header.textContent = "About Us"
+    firstText.innerHTML = "Welcome to <strong>Brainrot Bites</strong>, where We serve up dangerously delicious food that’ll fry your brain in the best way possible. <br> Whether you're here to satisfy your late-night munchies or just wanna vibe with the unhinged energy of our menu, we promise one thing, every bite is an out-of-body experience."
+    secondText.textContent = " So grab a seat, embrace the chaos, and scroll some reels."
 
-    content.appendChild(header)
-    content.appendChild(text)
+
+    aboutContainer.appendChild(header)
+    aboutTextContainer.appendChild(firstText)
+    aboutTextContainer.appendChild(secondText)
+    aboutContainer.appendChild(aboutTextContainer)
+    content.appendChild(aboutContainer)
 }
