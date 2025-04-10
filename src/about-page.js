bf4452817ea21e9602452a13de37b6304f8e4b89
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-04-10 20:29:33 +0530

    add linting

--- src/about-page.js
+++ src/about-page.js
@@ -10,7 +10,7 @@ export function getAboutPage(){
     const firstText = document.createElement("p")
     const secondText = document.createElement("p")
 
-    header.textContent = "About Us"
+    header.textContent = 'About Us'
     firstText.innerHTML = "Welcome to <strong>Brainrot Bites</strong>, where We serve up dangerously delicious food that’ll fry your brain in the best way possible. <br> Whether you're here to satisfy your late-night munchies or just wanna vibe with the unhinged energy of our menu, we promise one thing, every bite is an out-of-body experience."
     secondText.textContent = " So grab a seat, embrace the chaos, and scroll some reels."
 
